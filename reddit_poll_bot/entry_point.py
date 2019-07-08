b5612b27958c43079a579237821d518b2a4abb74
Joel Christiansen <greenbadge.jc@gmail.com>
2019-07-08 20:48:16 +0200

    adding in config parsing

--- reddit_poll_bot/entry_point.py
+++ reddit_poll_bot/entry_point.py
@@ -1,4 +1,5 @@
 import argparse
+from ConfigParser import ConfigParser
 
 from . import reddit_poll_bot
 
@@ -6,7 +7,12 @@ from . import reddit_poll_bot
 def build_parser():
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument('-c', '--config')
+    parser.add_argument('-t', '--title',
+                        help='the title of the reddit post')
     partial_args, _ = parser.parse_known_args()
+
+    parser.add_argument('choices', nargs='*')
+
     if partial_args.config is not None:
         return parser
     parser = argparse.ArgumentParser(parents=[parser])
@@ -20,15 +26,22 @@ def build_parser():
     parser.add_argument('-u', '--username')
     parser.add_argument('-p', '--password')
 
-    parser.add_argument('-t', '--title',
-                        help='the title of the reddit post')
-    parser.add_argument('choices', nargs='*')
     return parser
+
+
+def read_config_file(config_file):
+    parser = ConfigParser()
+    parser.read(config_file)
+    return dict(parser.items('reddit_poll_bot'))
 
 
 def create_client(args):
     if args.config is not None:
-        rpb = reddit_poll_bot.RedditPollBot(**read_config_file(parser.config))
+        config =read_config_file(args.config)
+        rpb = reddit_poll_bot.RedditPollBot(
+            user_agent='Reddit poll bot, in use by {}'.format(config['username']),
+            **config)
+
     else:
         rpb = reddit_poll_bot.RedditPollBot(
             username=args.username,
@@ -53,11 +66,13 @@ def create_post():
     post = rpb.create_poll(args.subreddit, args.title, args.body, args.choices)
     print(post.url)
 
+
 def update_post():
     parser = build_parser()
     args = parser.parse_args()
     rpb = create_client(args)
     rpb.post_votes(args.title, args.choices)
+
 
 def delete_post():
     parser = build_parser()
